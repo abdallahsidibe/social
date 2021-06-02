9525ebfa61530bd540b7b52751e9cc8008eac93c
abdallah monastir <sidibeabdallah01@gmail.com>
2021-06-02 19:45:31 +0100

    dzd

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -12,7 +12,8 @@
     <link rel="stylesheet" type="text/css" href="{{asset('assetss/css/bootstrap.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assetss/css/line-awesome.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assetss/css/line-awesome-font-awesome.min.css')}}">
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css')}}">
+    {{-- <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css')}}"> --}}
+    <script src="https://use.fontawesome.com/d03b8526a2.js"></script>
     <link rel="stylesheet" type="text/css" href="{{asset('assetss/css/font-awesome.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assetss/css/jquery.mCustomScrollbar.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('lib/slick/slick.css')}}">
